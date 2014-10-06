29466c6c6b70185fb92e459b81a5e129d77a0523
Deepak <deepak.as91@gmail.com>
2014-10-06 21:23:23 +0530

    updated configs

---
+++ config/configs.php
@@ -0,0 +1,18 @@
+<?php
+
+define('BASE_URL', 'http://localhost/violetphp/'); // must have trailing slash /
+
+
+define('DB_TYPE', 'mysql');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'simple_mvc');
+define('DB_USER', 'deepak');
+define('DB_PASS', 'password');
+
+define('DEFAULT_CONTROLLER', 'index');
+define('DEFAULT_METHOD', 'index');
+
+define('DEBUG_MODE', 'on');
+define('PAGE_404', 'error/404.php');
+
+define('TEMPLATE_ENGINE', 'on');
