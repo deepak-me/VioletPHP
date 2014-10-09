9660fc655eab65b1ec0f3646bb842abd7d1cfa13
Deepak <deepak.as91@gmail.com>
2014-10-09 19:35:20 +0530

    updated

--- config/configs.php
+++ config/configs.php
@@ -2,17 +2,23 @@
 
 define('BASE_URL', 'http://localhost/violetphp/'); // must have trailing slash /
 
+/*
+ * mysql , postgresql , sqlite
+ */
 
-define('DB_TYPE', 'mysql');
+define('DB_TYPE', 'postgresql');
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'simple_mvc');
-define('DB_USER', 'deepak');
+define('DB_NAME', 'testdb');
+define('DB_USER', 'postgres');
 define('DB_PASS', 'password');
+//for postgresql
+define('DB_PORT', '5432');
 
 define('DEFAULT_CONTROLLER', 'index');
 define('DEFAULT_METHOD', 'index');
 
 define('DEBUG_MODE', 'on');
+//turn off debug mode to acrivate error pages
 define('PAGE_404', 'error/404.php');
 
-define('TEMPLATE_ENGINE', 'on');
+define('TEMPLATE_ENGINE', 'on'); // turn on template engine to use teplate system
